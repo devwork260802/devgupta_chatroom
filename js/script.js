5b83a6097fbe3107687d5da81e72b4899aa603aa
Dev <devwork260802@gmail.com>
2023-05-07 09:56:17 +0530

    Dev Gupta Chatroom

---
+++ js/script.js
@@ -0,0 +1,49 @@
+const socket = io('http://localhost:8080');
+
+// Get DOM in JS Variables
+const form = document.getElementById('send-container');
+const messageInput = document.getElementById('messageInp')
+const messageContainer = document.querySelector(".container")
+
+// Audio that will play on receiving messages
+var audio = new Audio('../music/ting.mp3');
+
+// Function to append User Information to the container
+const append = (message, position)=>{
+    const messageElement = document.createElement('div');
+    messageElement.innerText = message;
+    messageElement.classList.add('message');
+    messageElement.classList.add(position);
+    messageContainer.append(messageElement);
+    if(position =='left'){ 
+        audio.play();
+    }
+}
+
+// Enter New User name to Join
+const name = prompt("Enter your name to join");
+socket.emit('new-user-joined', name);
+
+// Display name of New joined User
+socket.on('user-joined', name =>{
+    append(`${name} joined the chat`, 'right')
+})
+
+// If server sends a message, receive it
+socket.on('receive', data =>{
+    append(`${data.name}: ${data.message}`, 'left')
+})
+
+// If a user leaves the chat, append the info to the container
+socket.on('left', name =>{
+    append(`${name} left the chat`, 'right')
+})
+
+// If the form gets submitted, send server the message
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const message = messageInput.value;
+    append(`You: ${message}`, 'right');
+    socket.emit('send', message);
+    messageInput.value = ''
+})
